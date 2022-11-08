3de39697c5d3e3119515da3bb80a6644d86290f9
seo <seo@goldenplanet.co.kr>
2022-11-08 17:22:56 +0900

    scss 추가

--- front-end/pages/api/hello.js
+++ front-end/pages/api/hello.js
@@ -1,5 +1,26 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+// export default function handler(req, res) {
+//   res.status(200).json({ name: 'John Doe' })
+// }
+
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+
+function App() {
+   const [hello, setHello] = useState('')
+
+    useEffect(() => {
+        axios.get('/api/hello')
+        .then(response => setHello(response.data))
+        .catch(error => console.log(error))
+    }, []);
+
+    return (
+        <div>
+            (테스트용)백엔드에서 가져온 데이터입니다 : {hello}
+        </div>
+    );
 }
+
+export default App;
--- front-end/pages/user/join.js
+++ front-end/pages/user/join.js
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import axios from 'axios';
+import Image from "next/image";
+import logo from '../../asset/images/logo.png'
+
 export default function join() {
+    const {register, handleSubmit} = useForm();
+
     return (
-        <div>
-            회원가입
+        <div className="join">
+            <div className="join_title">
+                <Image src={logo}/>
+            </div>
+            <div className="join_contents">
+                <form>
+                    <label htmlFor="email">이메일</label>
+                    <input
+                    id="email"
+                    type="email"
+                    placeholder="test@email.com"
+                    {...register("email")}
+                    />
+                </form>
+            </div>
+
         </div>
     )
 }
