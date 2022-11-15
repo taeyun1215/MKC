50d4772c9f5aec068acbc042604ace9cb75773ca
seo <seo@goldenplanet.co.kr>
2022-11-15 13:59:16 +0900

    비밀번호 특수문자 조합으로 변경 및 이름을 닉네임으로 변경

--- front-end/pages/user/signup.js
+++ front-end/pages/user/signup.js
@@ -22,17 +22,17 @@ export default function signup() {
       .email("이메일 형식이 아닙니다."),
     password: yup
       .string()
-      .required("영문, 숫자포함 8자리를 입력해주세요.")
+      .required("영문, 숫자, 특수문자 포함 8자리를 입력해주세요.")
       .min(8, "최소 8자 이상 가능합니다")
-      .max(16, "최대 16자 까지만 가능합니다")
+      .max(16, "최대 20자 까지만 가능합니다")
       .matches(
-        /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,15}$/,
-        "영문 숫자포함 8자리를 입력해주세요."
+       /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[,./;'<>?:"~!@#$%^&*()])[a-zA-Z0-9,./;'<>?:"~!@#$%^&*()]{8,20}$/,
+        "영문, 숫자, 특수문자 포함 8자리를 입력해주세요."
       ),
     passwordConfirm: yup
       .string()
       .oneOf([yup.ref("password")], "비밀번호가 일치하지 않습니다."),
-    name: yup.string().required("이름은 필수 입력 정보입니다"),
+    name: yup.string().required("닉네임은 필수 입력 정보입니다"),
   });
 
   const {
@@ -45,7 +45,6 @@ export default function signup() {
   const onSubmit = (data) => {
     console.log("data", data);
   };
-
   return (
     <div className="signup">
       <div className="signup_title">
@@ -59,6 +58,7 @@ export default function signup() {
           type="text"
           {...register("id")}
           placeholder="아이디를 입력해주세요"
+          autoComplete="off"
         />
         {errors.id && <p>{errors.id.message}</p>}
         <label htmlFor="password">비밀번호</label>
@@ -66,7 +66,7 @@ export default function signup() {
           name="password"
           type="password"
           {...register("password")}
-          placeholder="비밀번호를 입력해주세요 (8 ~ 20자의 영문 및 숫자 조합)"
+          placeholder="비밀번호를 입력해주세요 (8 ~ 20자의 영문, 숫자, 특수문자 조합)"
         />
         {errors.password && <p>{errors.password.message}</p>}
         <label htmlFor="passwordConfirm">비밀번호 확인</label>
@@ -77,22 +77,25 @@ export default function signup() {
           placeholder="비밀번호를 한 번 더 입력해주세요"
         />
         {errors.passwordConfirm && <p>{errors.passwordConfirm.message}</p>}
+        <label htmlFor="name">닉네임</label>
+        <input
+          name="name"
+          type="text"
+          {...register("name")}
+          placeholder="닉네임을 입력해주세요"
+          autoComplete="off"
+        />
+        {errors.name && <p>{errors.name.message}</p>}
         <label htmlFor="email">이메일</label>
         <input
           name="email"
           type="email"
           placeholder="이메일을 입력해주세요"
           {...register("email")}
+          autoComplete="off"
         />
+        {/* <button>인증하기</button> */}
         {errors.email && <p>{errors.email.message}</p>}
-        <label htmlFor="name">이름</label>
-        <input
-          name="name"
-          type="text"
-          {...register("name")}
-          placeholder="이름을 입력해주세요"
-        />
-        {errors.name && <p>{errors.name.message}</p>}
         <button type="submit" className="signup_Btn">
           가입하기
         </button>
