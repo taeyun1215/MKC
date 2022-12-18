0d7a1703c3f9c1c22c5bb41f9dcc6a937ae9f738
seeun <tpdms0401@gmail.com>
2022-12-18 18:48:03 +0900

    상태관리 라이브러리 redux 추가

--- front-end/pages/_app.js
+++ front-end/pages/_app.js
@@ -2,8 +2,9 @@ import "../styles/main.scss";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import AppLayout from "../component/AppLayout";
+import { Provider } from "react-redux";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, store }) {
   const router = useRouter();
   return (
     <>
@@ -11,9 +12,12 @@ function MyApp({ Component, pageProps }) {
         <title>YEH</title>
       </Head>
       {router.pathname == "/user/signin" ||
-      router.pathname === "/user/signup" ? (
+      router.pathname === "/user/signup" ||
+      router.pathname === "/user/signupComplete" ? (
+        // <Provider store={store}>
         <Component {...pageProps} />
       ) : (
+        // </Provider>
         <AppLayout>
           <Component {...pageProps} />
         </AppLayout>
