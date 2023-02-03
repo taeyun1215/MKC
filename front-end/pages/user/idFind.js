979b28e21713c56fb6f1143ef6876ceed5cd22e7
seo <seo@goldenplanet.co.kr>
2023-02-03 16:30:35 +0900

    아이디 찾기 페이지 구현 완료

--- front-end/pages/user/idFind.js
+++ front-end/pages/user/idFind.js
@@ -1,11 +1,34 @@
-export default function idFind() {
+import axios from "axios"
+import { useState } from "react"
 
+export default function idFind() {
+    const [id, setId] = useState(null)
+    const handleOnChange = (event) =>{
+        setId(event.target.value)
+    }
+    const handleonKeyUp = (event) => {
+        if(event.keyCode === 13) {
+            handleOnSubmit()  
+        }
+    }
+    const handleOnSubmit = async() => {
+        const data = {
+            email : id
+        }
+        try {
+            const res = await axios.get('http://130.162.159.231:8080/api/username', {params: data})
+            if(res.data.success) alert('가입한 이메일 주소로 아이디가 전송되었습니다.')
+            else alert(res.data.error.message)
+        } catch(e) {
+            console.log(e)
+        }
+    }
     return (
-    <div className="sign" style={{ width: '25%' }}>
+    <div className="sign" style={{ width: '25%' , padding : '50px', border : '1px solid lightgray', borderRadius:'10px'}}>
         <div className="idFind">
             <span>가입한 이메일 주소를 입력주세요.</span>
-            <input placeholder="example@goldenplanet.co.kr"/>
-            <button>아이디 찾기</button>
+            <input placeholder="example@goldenplanet.co.kr" onChange={handleOnChange} onKeyUp={handleonKeyUp}/>
+            <button onClick={handleOnSubmit}>아이디 찾기</button>
         </div>
     </div>
     )
